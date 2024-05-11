b5bf1bf8cfb33ebcfc173458662e5da17b18a424
Moisés Eduardo <118224290+BeaterOfWar@users.noreply.github.com>
2024-05-11 17:16:21 -0300

    Update consulta_dados.sql

--- consulta_dados.sql
+++ consulta_dados.sql
@@ -1,14 +1,14 @@
---a. Dados completos de pessoas físicas.
+--a. Dados completos de pessoas fÃ­sicas.
 SELECT * FROM PessoaFisica PF
 INNER JOIN Pessoa P ON PF.Pessoa_idPessoa = P.idPessoa;
---b. Dados completos de pessoas jurídicas.
+--b. Dados completos de pessoas jurÃ­dicas.
 SELECT * FROM PessoaJuridica PJ
 INNER JOIN Pessoa P ON PJ.Pessoa_idPessoa = P.idPessoa;
---c. Movimentações de entrada, com produto,fornecedor, quantidade, preço unitário e valor total.
+--c. MovimentaÃ§Ãµes de entrada, com produto,fornecedor, quantidade, preÃ§o unitÃ¡rio e valor total.
 SELECT M.Produto_idProduto, M.Pessoa_idPessoa AS Fornecedor, M.quantidade, M.precoUnitario, M.quantidade * M.precoUnitario AS ValorTotal
 FROM Movimento M
 WHERE M.tipo = 'E';
---d. Movimentações de saída, com produto, comprador, quantidade, preço unitário e valor total.
+--d. MovimentaÃ§Ãµes de saÃ­da, com produto, comprador, quantidade, preÃ§o unitÃ¡rio e valor total.
 SELECT M.Produto_idProduto, M.Pessoa_idPessoa AS Comprador, M.quantidade, M.precoUnitario, M.quantidade * M.precoUnitario AS ValorTotal
 FROM Movimento M
 WHERE M.tipo = 'S';
@@ -17,26 +17,26 @@ SELECT M.Produto_idProduto, SUM(M.quantidade * M.precoUnitario) AS ValorTotal
 FROM Movimento M
 WHERE M.tipo = 'E'
 GROUP BY M.Produto_idProduto;
---Valor total das saídas agrupadas por produto.
+--f. Valor total das saÃ­das agrupadas por produto.
 SELECT M.Produto_idProduto, SUM(M.quantidade * M.precoUnitario) AS ValorTotal
 FROM Movimento M
 WHERE M.tipo = 'S'
 GROUP BY M.Produto_idProduto;
---f. Operadores que não efetuaram movimentações de entrada (compra).
+--g. Operadores que nÃ£o efetuaram movimentaÃ§Ãµes de entrada (compra).
 SELECT U.idUsuario
 FROM Usuario U
 WHERE U.idUsuario NOT IN (SELECT M.Usuario_idUsuario FROM Movimento M WHERE M.tipo = 'E');
---g. Valor total de entrada, agrupado por operador.
+--h. Valor total de entrada, agrupado por operador.
 SELECT M.Usuario_idUsuario, SUM(M.quantidade * M.precoUnitario) AS ValorTotal
 FROM Movimento M
 WHERE M.tipo = 'E'
 GROUP BY M.Usuario_idUsuario;
---h. Valor total de saída, agrupado por operador.
+--i. Valor total de saÃ­da, agrupado por operador.
 SELECT M.Usuario_idUsuario, SUM(M.quantidade * M.precoUnitario) AS ValorTotal
 FROM Movimento M
 WHERE M.tipo = 'S'
 GROUP BY M.Usuario_idUsuario;
---j. Valor médio de venda por produto, utilizando média ponderada.
+--j. Valor mÃ©dio de venda por produto, utilizando mÃ©dia ponderada.
 SELECT M.Produto_idProduto, SUM(M.quantidade * M.precoUnitario) / SUM(M.quantidade) AS PrecoMedioPonderado
 FROM Movimento M
 WHERE M.tipo = 'S'
